0f596c1ff72de13d86caaf26a3183fe2f9d78590
Kristian Fulkerson <kristianf451@gmail.com>
2022-01-10 18:25:24 -0800

    Finished homepage component, fetched all cryptos working, maping over data and mounting to dom

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const Cryptocurrencies = () => {
+  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  console.log(cryptos);
   return (
-    <div>
-      <h2>Crytpcurrencies Comp</h2>
-    </div>
+    <>
+      <Row gutters={[32, 32]} className="crypto-card-container">
+        {cryptos.map((currency) => (
+          <Col xs={24} sm={12} className="crypto-card" key={currency.uuid}>
+            <Link to={`/crypto/${currency.uuid}`}>
+              <Card title={`${currency.rank}. ${currency.name}`}></Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
